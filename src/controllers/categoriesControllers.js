ee0e497afca63ca16b97151ae59963d5a28a131d
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-21 16:05:04 -0300

    feat: valida o formato do body enviado a post em /categories

--- src/controllers/categoriesControllers.js
+++ src/controllers/categoriesControllers.js
@@ -1,7 +1,13 @@
 import connection from '../databases/postgres.js';
 
-export async function getCategories(req, res) {
+export async function getCategories(_, res) {
   const categories = await connection.query('SELECT * FROM categories;');
 
   res.status(200).send(categories.rows);
+}
+
+export async function setCategories(req, res) {
+  const { category } = res.locals;
+
+  res.sendStatus(200);
 }
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,8 +1,10 @@
-import { Router } from "express";
-import { getCategories } from "../controllers/categoriesControllers.js";
+import { Router } from 'express';
+import { getCategories, setCategories } from '../controllers/categoriesControllers.js';
+import categoryValidation from '../middlewares/categoryValidation.js';
 
 const router = Router();
 
-router.get("/categories", getCategories);
+router.get('/categories', getCategories);
+router.post('/categories', categoryValidation, setCategories);
 
 export default router;
