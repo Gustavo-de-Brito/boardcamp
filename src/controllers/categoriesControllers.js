b5aab904240edf0d6f06da108249d3be1b2e727e
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-21 16:38:49 -0300

    feat: valida se uma nova categoria ja nao esta registrada no db

--- src/controllers/categoriesControllers.js
+++ src/controllers/categoriesControllers.js
@@ -1,9 +1,13 @@
 import connection from '../databases/postgres.js';
 
 export async function getCategories(_, res) {
-  const categories = await connection.query('SELECT * FROM categories;');
-
-  res.status(200).send(categories.rows);
+  try {
+    const categories = await connection.query('SELECT * FROM categories;');
+  
+    res.status(200).send(categories.rows);
+  } catch(err) {
+    res.sendStatus(500);
+  }
 }
 
 export async function setCategories(req, res) {
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getCategories, setCategories } from '../controllers/categoriesControllers.js';
 import categoryValidation from '../middlewares/categoryValidation.js';
+import alreadyRegisteredCategory from '../middlewares/alredyRegisteredCategory.js';
 
 const router = Router();
 
 router.get('/categories', getCategories);
-router.post('/categories', categoryValidation, setCategories);
+router.post('/categories', categoryValidation, alreadyRegisteredCategory, setCategories);
 
 export default router;
