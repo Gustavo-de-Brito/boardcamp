e77d9bf21cbc2dacca4089f7763f6da20622a8d5
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-22 10:47:10 -0300

    feat: verifica se jogo enviado no post em /games ja nao esta no db

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getGames, setGame } from '../controllers/gamesControllers.js';
 import gameValidation from '../middlewares/gameValidation.js';
+import alreadyRegisteredGame from '../middlewares/alreadyRegisteredGame.js';
 
 const router = Router();
 
 router.get('/games', getGames);
-router.post('/games', gameValidation, setGame);
+router.post('/games', gameValidation, alreadyRegisteredGame, setGame);
 
 export default router;
