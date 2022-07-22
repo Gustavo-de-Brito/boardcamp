2bfa283ac2f63cfdf1ab991c950539cfd5062401
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-22 10:59:36 -0300

    feat: adiciona novo jogo ao db apos validacoes

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -40,5 +40,26 @@ export async function getGames(req, res) {
 }
 
 export async function setGame(req, res) {
-  res.sendStatus(200);
+  const {
+    name,
+    image,
+    stockTotal,
+    categoryId,
+    pricePerDay
+  } = req.body;
+
+  try {
+    await connection.query(
+      `
+      INSERT INTO games
+      ( name, image, "stockTotal", "categoryId", "pricePerDay" )
+      VALUES ( $1, $2, $3, $4, $5 );
+      `,
+      [ name, image, stockTotal, categoryId, pricePerDay ]
+    )
+
+    res.sendStatus(201);
+  } catch(err) {
+    res.sendStatus(500);
+  }
 }
--- src/middlewares/gameValidation.js
+++ src/middlewares/gameValidation.js
@@ -1,15 +1,29 @@
 import gameSchema from '../schema/gameSchema.js';
+import connection from '../databases/postgres.js';
 
-function gameValidation(req, res, next) {
+async function gameValidation(req, res, next) {
   const newGame = req.body;
 
   const { error } = gameSchema.validateAsync(newGame);
 
-  if(error) {
-    console.log(error.details);
-    return res.sendStatus(400);}
+  if(error) return res.sendStatus(400);
 
-  next();
+  try{
+    const { rows:isValidCategory } = await connection.query(
+      `
+      SELECT * FROM categories
+      WHERE id = $1;
+      `,
+      [ newGame.categoryId ]
+    );
+
+    if(isValidCategory.length === 0) return res.sendStatus(400);
+
+    next();
+  } catch(err) {
+    res.sendStatus(500);
+  }
+
 }
 
 export default gameValidation;
