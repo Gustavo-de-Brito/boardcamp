b6eee2b9deff4e7ab9b53faa78aeda7f16579acc
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-21 22:41:04 -0300

    feat: adiciona funcionalidade de buscar jogos pelo nome ou letra iniciais

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,15 +1,37 @@
 import connection from '../databases/postgres.js';
 
 export async function getGames(req, res) {
+  const { name } = req.query;
+
   try {
-    const { rows: games } = await connection.query(
-      `
-      SELECT games.*, categories.name as "categoryName"
-      FROM games
-      JOIN categories
-      ON games."categoryId" = categories.id
-      `
-    );
+    let games;
+
+    if(!name) {
+      const { rows } = await connection.query(
+        `
+        SELECT games.*, categories.name as "categoryName"
+        FROM games
+        JOIN categories
+        ON games."categoryId" = categories.id
+        `
+      );
+
+      games = rows;
+    } else {
+      const { rows } = await connection.query(
+        `
+        SELECT games.*, categories.name as "categoryName"
+        FROM games
+        JOIN categories
+        ON games."categoryId" = categories.id
+        WHERE LOWER(games.name)
+        LIKE LOWER($1)
+        `,
+        [ name + '%' ]
+        );
+
+        games = rows;
+      }
 
     res.status(200).send(games);
   } catch(err) {
