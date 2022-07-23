c2b859415cf5929980ee3e429cd162482e75cc6a
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-23 13:42:44 -0300

    feat: possiblita pesquisar clientes pelo cpf inteiro ou primeiros digitos

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -1,12 +1,33 @@
 import connection from '../databases/postgres.js';
 
 export async function getCustomers(req, res) {
-  try {
-    const { rows:customers } = await connection.query(
-      `SELECT * FROM customers`
-    );
+  const { cpf } = req.query;
 
-    res.status(200).send(customers);
+  try {
+    let customersList;
+
+    console.log(cpf);
+
+    if(cpf) {
+      const { rows:customers } = await connection.query(
+        `
+        SELECT * FROM customers
+        WHERE cpf
+        LIKE $1`
+        ,
+        [ cpf + '%' ]
+      );
+
+      customersList = customers;
+    } else {
+      const { rows:customers } = await connection.query(
+        `SELECT * FROM customers`
+      );
+
+      customersList = customers;
+    }
+
+    res.status(200).send(customersList);
   } catch(err) {
     res.sendStatus(500);
   }
