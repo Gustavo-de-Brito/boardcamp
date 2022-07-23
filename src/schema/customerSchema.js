41955bee25cf0b5f3b4c35db2a991e3fc1514f81
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-23 14:19:53 -0300

    feat: adiciona regras de formatacao para cpf, phone e date no post em /customers

--- src/schema/customerSchema.js
+++ src/schema/customerSchema.js
@@ -1,10 +1,13 @@
 import joi from 'joi';
 
+const phoneRegex = /^[0-9]{10,11}$/;
+const cpfRegex = /^[0-9]{11}$/;
+
 const customerSchema = joi.object(
   {
     name: joi.string().required(),
-    phone: joi.string().required(),
-    cpf: joi.string().required(),
+    phone: joi.string().pattern(phoneRegex).required(),
+    cpf: joi.string().pattern(cpfRegex).required(),
     birthday: joi.date().max('2022-01-01').min('1850-01-01').required()
   }
 );
